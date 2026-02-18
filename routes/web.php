ec0b3d90af39f3f9330cf89250245988c74bc1f9
AdolfoAugusto <adolfoaugusto2010@hotmail.com>
2026-02-18 00:28:19 -0300

    temporario

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/gerar-link-storage', function () {
+    try {
+        Artisan::call('storage:link');
+        return "Link criado com sucesso!";
+    } catch (\Exception $e) {
+        return "Erro ao criar link: " . $e->getMessage();
+    }
 });
