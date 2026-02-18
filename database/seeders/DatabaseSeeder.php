cf64caaf812b69fafabfb2d07cf1bf1c32c9f571
AdolfoAugusto <adolfoaugusto2010@hotmail.com>
2026-02-18 00:24:56 -0300

    ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Role;
+use App\Models\Company;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
